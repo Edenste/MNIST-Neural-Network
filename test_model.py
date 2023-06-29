09f45c101aa078b7f75d8770ef44793d88e35bde
Steven Pham <edensteven@protonmail.com>
2023-06-29 19:46:16 +1000

    Added handmade digits to test model

--- test_model.py
+++ test_model.py
@@ -15,6 +15,21 @@ x_test = tf.keras.utils.normalize(x_test, axis = 1)
 # Load model
 model = tf.keras.models.load_model('handwritten.model')
 
-# Test model
+# Test model against test data
 loss, accuracy = model.evaluate(x_test, y_test)
 print('Loss: ', str(loss), '\nAccuracy: ', accuracy)
+
+# Demonstrate prediction
+image_number = 1
+while os.path.isfile(f"digits/digit{image_number}.png"):
+    try:
+        img = cv2.imread(f"digits/digit{image_number}.png")[:,:,0]
+        img = np.invert(np.array([img]))
+        prediction = model.predict(img)
+        print(f"This digit is probably a {np.argmax(prediction)}") # Argmax returns the neuron that has the highest activation
+        plt.imshow(img[0], cmap = plt.cm.binary)
+        plt.show()
+    except:
+        print("An exception occurred")
+    finally:
+        image_number += 1
